855c4eabfd53d7e779cfc17a4638a92e0987a644
Igor <igor.wojda@gmail.com>
2020-08-20 22:21:08 +0200

    Update config

--- kotlin-start/app/src/main/java/com/vonage/tutorial/voice/Config.kt
+++ kotlin-start/app/src/main/java/com/vonage/tutorial/voice/Config.kt
@@ -9,11 +9,11 @@ object Config {
 
     val alice = User(
         "Alice",
-        "" // TODO: "set Alice's JWT token"
+        "ALICE_TOKEN" // TODO: "set Alice's JWT token"
     )
     val bob = User(
         "Bob",
-        "" // TODO: "set Bob JWT token"
+        "BOB_TOKEN" // TODO: "set Bob JWT token"
     )
 
     fun getOtherUserName(userName: String) = if (userName == alice.name) bob.name else alice.name
